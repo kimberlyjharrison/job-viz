f45e11f2f6c5bcf8148321aee46d20be2960a91e
kimberlyjharrison <kimberly.j.harrison@gmail.com>
2019-06-24 17:24:51 -0700

    dynamic

--- job_viz/app.py
+++ job_viz/app.py
@@ -13,11 +13,12 @@ from splinter import Browser
 from splinter.exceptions import ElementDoesNotExist
 from selenium import webdriver
 from pymongo import MongoClient
+from uszipcode import SearchEngine
 
 
 conn = os.environ.get('MONGODB_URI')
 if not conn:
-   	conn = "mongodb://localhost:27017/job_search_db"
+   	conn = "mongodb://localhost:27017/job_search_db2"
 
 app = Flask(__name__)
 
@@ -38,165 +39,148 @@ def json_api():
 		data.append(x)
 	return jsonify(data)
 
-@app.route('/<job_input>')
-def scraper(job_input):
+@app.route('/search/<job_title>')
+def search_and_map(job_title):
+
+	job_titles = []
+	company_names = []
+	locations = []
+	salaries = []
+	link_list = []
+
+	url = f'https://www.indeed.com/jobs?q={job_title}&limit=50'
+
+	def scrapeData(class_str, list_name):
+		element = soup.find_all(class_=class_str)
+		for i in element:
+			list_name.append(i.text.strip())
+
+	def scrapeSalary(num, list_name):
+		salary_tags = soup.find_all(class_='row')
+		for i in range(len(salary_tags)):
+			try:
+				salary = salary_tags[i].find(class_='salary').text.strip()
+				list_name.append(salary)
+			except:
+				list_name.append("No Salary Info Provided")
+
+	def scrapeLink(list_name):
+		frontend = 'https://www.indeed.com/'
+		links = soup.find_all('div', class_='title')
+		for link in links:
+			backend = link.find('a')['href']
+			list_name.append(frontend+backend)
 	
-	def scrapeIndeed(job_title):
+	def getZip(str, zipList):
+		if str.isdigit():
+			zipList.append(str)
+		else:
+			try:
+				search = SearchEngine(simple_zipcode=True)
+				city_state_str = str.split(', ')
+				city_state = search.by_city_and_state(city_state_str[0], city_state_str[1])
+				zipList.append(city_state[0].zipcode)
+			except:
+				zipList.append("None")
+				#print(f"No City, State info found for {str}")
 
-		job_titles = []
-		company_names = []
-		locations = []
-		salaries = []
-		link_list = []
+	CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
 
-		
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.binary_location = '.apt/usr/bin/google-chrome-stable'
+	chrome_options.add_argument('--disable-gpu')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('headless')
+	browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+	browser.get(url)
+	html = browser.page_source
+	soup = bs(html, 'html.parser')
 
-		url = f'https://www.indeed.com/jobs?q={job_title}&limit=50'
+	scrapeData('title', job_titles)
+	scrapeData('company', company_names)
+	scrapeData('location', locations)
+	num = len(job_titles)
+	scrapeSalary(num, salaries)
+	scrapeLink(link_list)
 
-		
-		def scrapeData(class_str, list_name):
-			element = soup.find_all(class_=class_str)
-			for i in element:
-				list_name.append(i.text.strip())
+	def hasZip(inputString):
+		if any(char.isdigit() for char in inputString):
+			return "".join(filter(lambda x: x.isdigit(), inputString))
+		else:
+			return inputString
 
-		def scrapeSalary(num, list_name):
-			salary_tags = soup.find_all(class_='row')
-			for i in range(len(salary_tags)):
-				try:
-					salary = salary_tags[i].find(class_='salary').text.strip()
-					list_name.append(salary)
-				except:
-					list_name.append("No Salary Info Provided")
+	loc_list = []
+	for i in locations:
+		loc_list.append(hasZip(i))
 
-		def scrapeLink(list_name):
-			frontend = 'https://www.indeed.com/'
-			links = soup.find_all('div', class_='title')
-			for link in links:
-				backend = link.find('a')['href']
-				list_name.append(frontend+backend)
-		
-		def getZip(str, zipList):
-			if str.isdigit():
-				zipList.append(str)
-			else:
-				try:
-					search = SearchEngine(simple_zipcode=True)
-					city_state_str = str.split(', ')
-					city_state = search.by_city_and_state(city_state_str[0], city_state_str[1])
-					zipList.append(city_state[0].zipcode)
-				except:
-					zipList.append("None")
-					print(f"No City, State info found for {str}")
+	zip_list = []
+	for i in loc_list:
+		getZip(i, zip_list)
 
+	def getCoord(str, coordList, cityList):
+		search = SearchEngine(simple_zipcode=True)
+		if str.isdigit():
+			try:
+				zipcode = search.by_zipcode(str)
+				coords = [zipcode.lat, zipcode.lng]
+				coordList.append(coords)
+				cityList.append(zipcode.post_office_city)
+			except:
+				coordList.append(["None", "None"])
+				cityList.append("None")
+				#print(f"No Results Found for Zip {str}")
+		else:
+			coordList.append(["None", "None"])
+			cityList.append(str)
+	
+	search_limit = num
 
-		CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
-		
-		chrome_options = webdriver.ChromeOptions()
-		
-		chrome_options.binary_location = '.apt/usr/bin/google-chrome-stable'
-		chrome_options.add_argument('--disable-gpu')
-		chrome_options.add_argument('--no-sandbox')
-		chrome_options.add_argument('headless')
-		
-		browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-		# executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-		# browser = Browser('chrome', **executable_path, headless=True)
-		#browser.visit(url)
-		browser.get(url)
-		# html = browser.html
-		html = browser.page_source
-		soup = bs(html, 'html.parser')
+	coord_list = []
+	city_list = []
 
-		scrapeData('title', job_titles)
-		scrapeData('company', company_names)
-		scrapeData('location', locations)
-		num = len(job_titles)
-		scrapeSalary(num, salaries)
-		scrapeLink(link_list)
+	for i in zip_list[0:search_limit]:
+		getCoord(i, coord_list, city_list)
 
-		browser.quit()
+	job_list = []
 
-
-		def hasZip(inputString):
-			if any(char.isdigit() for char in inputString):
-				#return inputString[-5:]
-				return "".join(filter(lambda x: x.isdigit(), inputString))
-			else:
-				return inputString
-
-
-		loc_list = []
-		for i in locations:
-			loc_list.append(hasZip(i))
-
-		#zip_list = []
-		# for i in loc_list:
-		#     getZip(i, zip_list)
-
-		# def getCoord(str, coordList, cityList):
-		#     search = SearchEngine(simple_zipcode=True)
-		#     if str.isdigit():
-		#         try:
-		#             zipcode = search.by_zipcode(str)
-		#             coords = [zipcode.lat, zipcode.lng]
-		#             coordList.append(coords)
-		#             cityList.append(zipcode.post_office_city)
-		#         except:
-		#             coordList.append(["None", "None"])
-		#             cityList.append("None")
-		#             print(f"No Results Found for Zip {str}")
-		#     else:
-		#         coordList.append(["None", "None"])
-		#         cityList.append(str)
-		
-		
-
-		# coord_list = []
-		# city_list = []
-
-		# for i in zip_list[0:search_limit]:
-		#     getCoord(i, coord_list, city_list)
-
-		search_limit = 50
-		job_list = []
-
-		for i in range(search_limit):
+	for i in range(search_limit):
+		try:
 			job_dict = {}
 			job_dict['Title'] = job_titles[i]
 			job_dict['Company'] = company_names[i]
-			job_dict["Locations"] = loc_list[i]
-			#job_dict['Location'] = city_list[i]
-			#job_dict["Coordinates"] = coord_list[i]
+			job_dict["ZipCode"] = zip_list[i]
+			job_dict['Location'] = city_list[i]
+			job_dict["Coordinates"] = coord_list[i]
 			job_dict["Salary_Info"] = salaries[i]
 			job_dict['Link'] = link_list[i]
+			job_dict['Search_Term']=job_title
 			job_list.append(job_dict)
+		except:
+			break
 
-		# for i in job_list:
-		#     if "None" in str(i["Coordinates"]):
-		#         job_list.remove(i)
+	for i in job_list:
+		if i["Coordinates"] == ['None', 'None']:
+			job_list.remove(i)
+			
+	client = MongoClient(conn)
+	db = client['job_search_db2']
 
-		# conn = 'mongodb://localhost:27017'
-		# client = MongoClient(conn)
-		# db = client['job_search_db']
+	db.search_results.drop()
 
-		# db.search_results.drop()
+	col = db['search_results']
 
-		# col = db['search_results']
+	for x in job_list:
+		if isinstance(x['Coordinates'][0], float):
+			col.insert_one(x)
 
-		# for x in job_list:
-		#     col.insert_one(x)
-		
-		return(job_list)
+	data = []
 
+	query = {'Search_Term': job_title}
+	for x in mongo.db.search_results.find(query):
+		x.pop('_id')
+		data.append(x)
 
-
-
-
-	jobs_data = scrapeIndeed(job_input)
-	mongo.db.search_results.drop()
-	jobs = mongo.db.search_results
-	jobs.insert_many(jobs_data)
-	return redirect("/", code=302)
+	return redirect('/', code=302)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- job_viz/static/js/logic.js
+++ job_viz/static/js/logic.js
@@ -1,150 +1,155 @@
-console.log("Shout out to Chris! JavaScript RULEZ!")
+console.log('logic.js working')
 
-//initialize end points for queries for earthquake data and fault lines
-var quakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
-var faultsUrl = 'https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json'
+var searchTerm
 
-// Perform a GET request to the earthquake query URL
-d3.json(quakeUrl, function(data) {
-  //send response to createFeatures function
-  createFeatures(data.features);
-});
-
-//fucntion to create earthquake markers and fault data
-function createFeatures(earthquakeData) {
-
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + "Magnitude " + feature.properties.mag + "<br>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
-
-  //create function to add circles to map based on magnitude of earthquake
-  function pointToLayer(feature, latlng) {
-  	return L.circleMarker(latlng, {
-  		  	radius: scaleRadius(feature.properties.mag),
-  			fillColor: getColor(feature.properties.mag),
-  			fillOpacity: 0.7,
-  			weight: .5,
-  			color: 'black'
-  	});
-  }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  // Reun pointToLayer function to add cirlces to map
-  var earthquakes = L.geoJSON(earthquakeData, {
-  	pointToLayer: pointToLayer,
-    onEachFeature: onEachFeature
-  });
+$('#inputText').attr('placeholder', searchTerm)
 
 
-  //perform GET request on fault info
-  d3.json(faultsUrl, function(data) {
-
-  	//create fault variable to store geoJSON data
-  	var faults = L.geoJSON(data.features, {
-  		fillOpacity: 0,
-  		color: 'green'
-  	});
-
-  	//call create map function with earthquake data, fault data
-  	createMap(earthquakes, faults);
-  })
+function loader() {
+  $('#mapText').after('<div class="loader"></div>')
 }
 
-//Function to render map
-function createMap(earthquakes, faults) {
+function enterSearch() {
+    // Fetch new data each time a new sample is selected
 
-  // Define satelite layer
-  var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-    maxZoom: 18,
-    id: "mapbox.sat",
-    accessToken: API_KEY
-  });
+     loader()
+    $ ("#scrapeText").empty()
+    $ ('#mapText').empty()
 
-  // Define dark map layer
-  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.dark",
-    accessToken: API_KEY
-  });
+    var searchurl = 'https://job-viz.herokuapp.com/search/'+$('#inputText').val() 
+    $(location).attr('href', searchurl)
+    $ ("#scrapeText").append('<h3>Gathering Data for Search Term: "' + $('#inputText').val() + '"</h3>') 
+  };
 
-  // Define streetmap layer
-  var Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
-	maxZoon: 18,
-	id: 'mapbox.street',
-	accessToken: API_KEY
-	});
+function mapResults() {
+  $ ('#mapText').empty()
+   $ ("#scrapeText").empty()
+  const apiurl = 'https://job-viz.herokuapp.com/api'
 
-  // Define a baseMaps object to hold  base layers
-  var baseMaps = {
-    "Satelite": Esri_WorldImagery,
-    "Street Map": Esri_WorldStreetMap,
+ d3.json(apiurl, function(data) {
+   var searchTerm = data[0].Search_Term
+   $ ("#mapText").append('<h3>Showing Data for Search Term: "' + searchTerm + '"</h3>') 
+ })
+ 
+  var data = d3.json(apiurl, function(data) {
+  createPoints(data)
+})
+};
+
+
+var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+});
+
+var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
+
+var baseMaps = {
+    "Street Map": streetmap,
     "Dark Map": darkmap
   };
 
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    'Earthquakes': earthquakes,
-    'Fault Lines': faults
-  };
-
-  // Create map object centered on USA with zoom to world level
-  var map = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 3,
-    layers: [Esri_WorldImagery, earthquakes, faults]
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5,
+    layers: [streetmap]
   });
 
-  // Create legend (reference: https://leafletjs.com/examples/choropleth/)
-  var legend = L.control({position: 'bottomright'});
+function createPoints(data) {
 
-	legend.onAdd = function (map) {
+  map.remove()
+  $('#mapholder').after('<div id="map"></div>')
+  
 
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 1, 2, 3, 4, 5],
-    	labels = [];
+  var jobPostingsArray = [];
+  for (var i = 0; i < data.length; i++) {
+      var jobPosting = data[i];
+      if (jobPosting.Coordinates && jobPosting.Location) {
+          jobPostingsArray.push(L.marker(jobPosting.Coordinates)
+              .bindPopup("<h2>" + jobPosting.Title + "</h2>" + "<h3>" + jobPosting.Company + "</h3> <hr> <h4>" + jobPosting.Location +
+              "</h4> <h5>" + jobPosting.Salary_Info + "</h5> <a href=\"" + jobPosting.Link + "\" target=\"_blank\">Find Job Posting Here</a>"));
+      }
+  };
 
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
+  var heatArray = [];
+  for (var i = 0; i < data.length; i++) {
+      var jobPosting = data[i];
 
-    return div;
-	};
+      if (jobPosting.Location) {
+        heatArray.push(jobPosting.Coordinates);
+      };
 
-  // Add lgend to map
-  legend.addTo(map);
+      var heat = L.heatLayer(heatArray, {
+        radius: 80,
+        blur: 28,
+        minOpacity: 0.55
+      });
+  };
 
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
+  var zillowArray = [];
+  for (var i = 0; i < zillow.length; i++) {
+
+      if (zillow[i].lat && zillow[i].lng) {
+          zillowArray.push([zillow[i].lat, zillow[i].lng])
+      }
+
+      var zillowHeat = L.heatLayer(zillowArray, {
+          radius: 10,
+          blur: 1,
+          minOpacity: 0.2,
+          gradient: {
+              '.2': 'blue',
+              '.4': 'green',
+              '.6': 'yellow',
+              '.8': 'orange',
+              '.99': 'red'
+          },
+      });
+  };
+
+
+  jobPostingsLayer = L.layerGroup(jobPostingsArray);
+
+  var overlayMaps = {
+    "Job Postings": jobPostingsLayer,
+    "HeatMap of Job Posts": heat,
+    "Median $/sqft (Housing)": zillowHeat
+  };
+
+
+  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+});
+
+var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
+
+var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+  var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 4.5,
+    layers: [streetmap, heat, jobPostingsLayer]
+  });
+
+
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
-  }).addTo(map);
-}
-
-
-// Function to define color based on magnitude of earthquake
-function getColor(d) {
-    return d > 5 ? '#800026' :
-           d > 4 ? '#BD0026' :
-           d > 3 ? '#E31A1C' :
-           d > 2 ? '#FC4E2A' :
-           d > 1 ? '#FD8D3C' :
-				   '#FFEDA0';
-}
-
-// Scale radius to show on map
-function scaleRadius(r) {
-	return r*4
+  }).addTo(myMap);
 }
